8ef1d0f567cdfd4d6b62a38ed385e6bf75c4a3fe
Julia Seidman <juliaseid@gmail.com>
2020-07-02 20:10:33 -0700

    Add Flavor List component

---
+++ src/components/FlavorList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Flavor from './Flavor';
+import PropTypes from 'prop-types';
+
+function FlavorList(props) {
+
+  return (
+    <React.Fragment>
+      <hr />
+      <hr />
+      {props.flavorList.map((flavor) =>
+        <Flavor
+          name={flavor.name}
+          allergens={flavor.allergens}
+          size={flavor.size}
+          scoops={flavor.scoops}
+          id={flavor.id} />
+      )}
+    </React.Fragment>
+  );
+}
+
+FlavorList.propTypes = {
+  flavorList: PropTypes.array
+};
+
+export default FlavorList;

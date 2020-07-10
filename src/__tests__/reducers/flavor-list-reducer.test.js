1c0cefbb5e7a90c928d44b335acdb4076cdc3de9
Julia Seidman <juliaseid@gmail.com>
2020-07-10 10:03:15 -0700

    add flavor test & functionality

--- src/__tests__/reducers/flavor-list-reducer.test.js
+++ src/__tests__/reducers/flavor-list-reducer.test.js
@@ -1,7 +1,38 @@
 import flavorListReducer from '../../reducers/flavor-list-reducer';
 
 describe('flavorListReducer', () => {
+
+  let action;
+  const flavorData = {
+    name: 'hazelnut salted caramel',
+    allergens: 'tree nuts, dairy',
+    creamery: 'Tillamook',
+    id: 1
+  };
+
+  test('Should successfully add new flavor data to masterFlavorList', () => {
+    const { name, allergens, creamery, id } = flavorData;
+    action = {
+      type: 'ADD_FLAVOR',
+      name: name,
+      allergens: allergens,
+      creamery: creamery,
+      id: id
+    };
+
+    expect(flavorListReducer({}, action)).toEqual({
+      [id]: {
+        name: name,
+        allergens: allergens,
+        creamery: creamery,
+        id: id
+      }
+    });
+  });
+
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(flavorListReducer({}, { type: null })).toEqual({});
   });
+
 });
